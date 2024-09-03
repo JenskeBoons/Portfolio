989afbe1b7676ab259d5b5287f92ea2091f9b8f4
unknown <r0889615@student.thomasmore.be>
2024-09-03 23:39:16 +0200

    diploma

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -18,7 +18,7 @@ const About: NextPage = () => {
                     Hallo&#44; ik ben <span>Jens Boons</span>
                   </h2>
                   <p>
-                    Momenteel studeer ik <strong>Toegepaste Informatica</strong> aan Thomas More Geel. Ik vind het leuk om iets over mezelf en mijn <strong>passie voor programmeren</strong> met jullie te delen.
+                    Momenteel ben ik <strong>afgestudeerd</strong> met bachelor <strong>Toegepaste Informatica</strong> aan Thomas More Geel. Ik vind het leuk om iets over mezelf en mijn <strong>passie voor programmeren</strong> met jullie te delen.
                   </p>
                   <p>Al sinds mijn jeugd ben ik <strong>gefascineerd door technologie</strong> en de manier waarop deze ons dagelijks leven beïnvloedt. Toen ik ouder werd, ontdekte ik mijn liefde voor podiumverlichting en programmering, en de eindeloze mogelijkheden die dit biedt.</p>
                   <p>
@@ -167,7 +167,7 @@ const About: NextPage = () => {
                     <div className="skill-item padd-15">
                       <h5>.NET</h5>
                       <div className="progress">
-                        <div className="progress-in" style={{ width: "40%" }} />
+                        <div className="progress-in" style={{ width: "55%" }} />
                         <div className="skill-percent">40%</div>
                       </div>
                     </div>
